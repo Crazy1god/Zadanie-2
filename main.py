4e559efa9612eec70cdd3e41f3e2ed312b532ec1
Crazy1god <11212roma121211@gmail.com>
2024-12-13 20:52:33 +0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print ("1st program", 9**0.5*5)
 print ("2nd program", 9.99 > 9.98 and 1000 != 999)
 print ("3rd program", 2*2+2,2*(2+2), 2*2+2==2*(2+2))
-print ("4th program", (' 123.465'), (int(1234.56)%10))
+print ("4th program", (' 123.465'), ((int(123.456*10)%10)))
